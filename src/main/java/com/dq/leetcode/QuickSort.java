ebe2040e2018ca6ac68948cfc0f73ee11daefc36
daquan <15197463299@163.com>
2020-03-24 22:46:16 +0800

    update quickSort

--- src/main/java/com/dq/leetcode/QuickSort.java
+++ src/main/java/com/dq/leetcode/QuickSort.java
@@ -8,37 +8,40 @@ package com.dq.leetcode;
  */
 public class QuickSort {
 
-    public static final void quickSort(int[] num, int left, int right) {
+    /**
+     * @param arr   数组
+     * @param left  数组最左边元素
+     * @param right 数组最右边元素
+     */
+    public static final void quickSort(int[] arr, int left, int right) {
         //如果left等于right，即数组只有一个元素，直接返回
         if (left >= right) {
             return;
         }
         //设置最左边的元素为基准值
-        int key = num[left];
+        int key = arr[left];
         //数组中比key小的放在左边，比key大的放在右边，key值下标为i
-        int i = left;
-        int j = right;
+        int i = left, j = right;
         while (i < j) {
             //j向左移，直到遇到比key小的值
-            while (num[j] >= key && i < j) {
+            while (arr[j] >= key && i < j) {
                 j--;
             }
             //i向右移，直到遇到比key大的值
-            while (num[i] <= key && i < j) {
+            while (arr[i] <= key && i < j) {
                 i++;
             }
             //i和j指向的元素交换
             if (i < j) {
-                int temp = num[i];
-                num[i] = num[j];
-                num[j] = temp;
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
             }
         }
-        num[left] = num[i];
-        num[i] = key;
-        quickSort(num, left, i - 1);
-        quickSort(num, i + 1, right);
-
+        arr[left] = arr[i];
+        arr[i] = key;
+        quickSort(arr, left, i - 1);
+        quickSort(arr, i + 1, right);
     }
 
 
